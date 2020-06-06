fab5c4ab144c7ee21f710199f371edfef1dad135
Libikk <libikpl@gmail.com>
2020-06-06 19:29:02 +0100

    chore: button success variant

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const buttonStyle = {
-  margin: '10px 10px 10px 0',
-};
+const ButtonStyles = styled.button`
+  cursor: pointer;
+  background: ${({ theme }) => theme.color.green[500]};
+  font-size: 16px;
+  border-radius: 3px;
+  color: ${({ theme }) => theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.green[500]};
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+  font-weight: ${({ theme }) => theme.font.weight.medium};
 
-const Button = (props) => {
-  const { handleClick, children } = props;
-  return <button {...props} style={buttonStyle} onClick={handleClick}>{children}</button>;
-};
+  &:hover {
+    background: ${({ theme }) => theme.color.green[600]};
+    border-color: ${({ theme }) => theme.color.green[500]};
+    color: #d8d8d8;
+  }
+`;
+
+const Button = ({ children, ...rest }) => <ButtonStyles {...rest}>{children}</ButtonStyles>;
 
 
 export default Button;
