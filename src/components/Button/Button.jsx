0f45b029e231668c30245665c1c0f9e97c5e22a1
Libikk <libikpl@gmail.com>
2020-06-07 10:48:15 +0100

    chore: buttons themes init

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,29 +1,69 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import themes from 'styled-theming';
+import PropTypes from 'prop-types';
+
+const backgroundBorderColor = themes('variant', {
+  default: ({ theme }) => theme.color.grey[300],
+  success: ({ theme }) => theme.color.green[500],
+});
+
+const hoverBorderColor = themes('variant', {
+  default: ({ theme }) => theme.color.black,
+  success: ({ theme }) => theme.color.green[500],
+});
+
+const hoverBgColor = themes('variant', {
+  default: ({ theme }) => theme.color.grey[500],
+  success: ({ theme }) => theme.color.green[600],
+});
+
+const hoverColor = themes('variant', {
+  default: ({ theme }) => theme.color.black,
+  success: ({ theme }) => theme.color.grey[400],
+});
+
+const color = themes('variant', {
+  default: ({ theme }) => theme.color.black,
+  success: ({ theme }) => theme.color.white,
+});
+
 
 const ButtonStyles = styled.button`
   cursor: pointer;
-  background: ${({ theme }) => theme.color.green[500]};
+  background: ${backgroundBorderColor};
   font-size: 16px;
   border-radius: 3px;
-  color: ${({ theme }) => theme.color.white};
-  border: 1px solid ${({ theme }) => theme.color.green[500]};
-  margin: 0 1em;
+  color: ${color};
+  border: 1px solid ${backgroundBorderColor};
   padding: 0.25em 1em;
   transition: 0.5s all ease-out;
   font-weight: ${({ theme }) => theme.font.weight.medium};
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
-  
+
   &:hover {
-    background: ${({ theme }) => theme.color.green[600]};
-    border-color: ${({ theme }) => theme.color.green[500]};
-    color: #d8d8d8;
+    background: ${({ disabled }) => (disabled ? '' : hoverBgColor)};
+    border-color: ${hoverBorderColor};
+    color: ${({ disabled }) => (disabled ? '' : hoverColor)};
   }
 `;
 
-const Button = ({ children, isLoading, ...rest }) => {
+ButtonStyles.defaultProps = {
+  variant: 'default',
+};
+
+ButtonStyles.propTypes = {
+  variant: PropTypes.oneOf(['default', 'primary', 'success', 'warning']),
+};
+
+const Button = ({ children, isLoading, variant = 'default', ...rest }) => {
   const loadingComponent = 'Loading...';
-  return <ButtonStyles {...rest}>{isLoading ? loadingComponent : children}</ButtonStyles>;
+  return (
+    <ThemeProvider theme={{ variant }}>
+      <ButtonStyles {...rest}>{isLoading ? loadingComponent : children}</ButtonStyles>
+      ;
+    </ThemeProvider>
+  );
 };
 
 
