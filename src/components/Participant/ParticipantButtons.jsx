6854936bebc3a279eecfe6bdb337f011c9b13d87
Libikk <libikpl@gmail.com>
2020-07-18 16:05:04 +0100

    refactor: split component into smaller

--- src/components/Participant/ParticipantButtons.jsx
+++ src/components/Participant/ParticipantButtons.jsx
@@ -12,42 +12,47 @@ const Buttons = styled.div`
   left: 200px;
   z-index: 1;
 `;
-const ParticipantButtons = ({ myself, handleLogout }) => {
+const MyButtons = ({ handleLogout }) => {
   const [isMyMicrophoneMuted, setIsMyMicrophoneMuted] = useState(false);
   const [isVideoOn, setIsVideoOn] = useState(true);
-  const [isParticipantMuted, setIsParticipantMuted] = useState(true);
   return (
-    <Buttons>
-      {myself
-        ? (
-          <>
-            <Button Icon={IoMdWalk} color="red" onClick={handleLogout} title="Exit the room" />
-            <Button Icon={IoMdArrowForward} color="green" title="Search next room" />
-            <Button
-              Icon={isMyMicrophoneMuted ? IoIosMicOff : IoIosMic}
-              onClick={() => setIsMyMicrophoneMuted(!isMyMicrophoneMuted)}
-              title={isMyMicrophoneMuted ? 'Unmute microphone' : 'Mute microphone'}
-            />
-            <Button
-              Icon={isVideoOn ? RiCameraOffLine : RiCameraLine}
-              onClick={() => setIsVideoOn(!isVideoOn)}
-              title={isVideoOn ? 'Hide camera' : 'Show camera'}
-            />
-          </>
-        ) : (
-          <>
-            <Button
-              Icon={isParticipantMuted ? GiSpeaker : GiSpeakerOff}
-              onClick={() => setIsParticipantMuted(!isParticipantMuted)}
-              title={isParticipantMuted ? 'Mute this user' : 'Unmute this user'}
-            />
-            <Button Icon={IoIosRemoveCircleOutline} onClick={handleLogout} title="Initialize vote to kick user" />
-            <Button Icon={AiOutlineExclamation} title="Report user" />
-            <Button Icon={IoIosSend} title="Send privet message" />
-          </>
-        )}
-    </Buttons>
+    <>
+      <Button Icon={IoMdWalk} color="red" onClick={handleLogout} title="Leave" />
+      <Button Icon={IoMdArrowForward} color="green" title="Next room" />
+      <Button
+        Icon={isMyMicrophoneMuted ? IoIosMicOff : IoIosMic}
+        onClick={() => setIsMyMicrophoneMuted(!isMyMicrophoneMuted)}
+        title={isMyMicrophoneMuted ? 'Unmute' : 'Mute'}
+      />
+      <Button
+        Icon={isVideoOn ? RiCameraOffLine : RiCameraLine}
+        onClick={() => setIsVideoOn(!isVideoOn)}
+        title={isVideoOn ? 'Hide camera' : 'Show camera'}
+      />
+    </>
   );
 };
+
+const OtherParticipantButtons = () => {
+  const [isParticipantMuted, setIsParticipantMuted] = useState(true);
+  return (
+    <>
+      <Button
+        Icon={isParticipantMuted ? GiSpeaker : GiSpeakerOff}
+        onClick={() => setIsParticipantMuted(!isParticipantMuted)}
+        title={isParticipantMuted ? 'Mute' : 'Unmute'}
+      />
+      <Button Icon={IoIosRemoveCircleOutline} title="Initialize vote to kick user" />
+      <Button Icon={AiOutlineExclamation} title="Report user" />
+      <Button Icon={IoIosSend} title="Send privet message" />
+    </>
+  );
+};
+
+const ParticipantButtons = ({ myself, handleLogout }) => (
+  <Buttons>
+    {myself ? <MyButtons handleLogout={handleLogout} /> : <OtherParticipantButtons />}
+  </Buttons>
+);
 
 export default ParticipantButtons;
