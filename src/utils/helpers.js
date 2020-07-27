2d1073fc78f6bd6359d63c27c0365cf8311afe48
Libikk <libikpl@gmail.com>
2020-07-27 11:34:33 +0100

    chore: add vs code sttings and fix lint

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,10 @@
 export const getQueryVariable = (variable) => {
-    const query = window.location.search.substring(1);
-    const vars = query.split('&');
-    const varsWithValues = vars.reduce((acc, pair) => {
-        const [key, value] = pair.split('=');
-         return { ...acc, [decodeURIComponent(key)]: decodeURIComponent(value) }
-    }, {})
+  const query = window.location.search.substring(1);
+  const vars = query.split('&');
+  const varsWithValues = vars.reduce((acc, pair) => {
+    const [key, value] = pair.split('=');
+    return { ...acc, [decodeURIComponent(key)]: decodeURIComponent(value) };
+  }, {});
 
-    return varsWithValues[variable] || null;
-  }
+  return varsWithValues[variable] || null;
+};
