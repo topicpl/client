a72c9a7a83cbcf6be7a4078468ec9723393e4677
Libikk <libikpl@gmail.com>
2020-07-27 11:22:52 +0100

    helpers

---
+++ src/utils/helpers.js
@@ -0,0 +1,10 @@
+export const getQueryVariable = (variable) => {
+    const query = window.location.search.substring(1);
+    const vars = query.split('&');
+    const varsWithValues = vars.reduce((acc, pair) => {
+        const [key, value] = pair.split('=');
+         return { ...acc, [decodeURIComponent(key)]: decodeURIComponent(value) }
+    }, {})
+
+    return varsWithValues[variable] || null;
+  }
