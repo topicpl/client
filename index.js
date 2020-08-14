7a0ece0e0a3d1aea5b00a8e4fd338f215b6038ac
libikk <libikpl@gmail.com>
2020-08-14 10:26:25 +0200

    lint fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3001, () => console.log('The Topic app listening on port 3001!'));
+app.listen(3001, () => console.warn('The Topic app listening on port 3001!'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: path.join(__dirname, '/public'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: path.join(__dirname, '/public'),
-        historyApiFallback: true,
-        inline: true,
-        port: 3001,
-        hot: true,
-    },
-    module: {
-        rules: [{
-            test: /\.js|jsx$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            resolve: {
-                extensions: ['.js', '.jsx'],
-            },
-            query: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
-        }, ],
-    },
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/public'),
+    historyApiFallback: true,
+    inline: true,
+    port: 3001,
+    hot: true,
+  },
+  module: {
+    rules: [{
+      test: /\.js|jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      resolve: {
+        extensions: ['.js', '.jsx'],
+      },
+      query: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+      },
+    }],
+  },
 };
