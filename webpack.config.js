e87bb33160a1159d2bcaca1e5b4b669e2c44c132
libikk <libikpl@gmail.com>
2020-08-14 10:18:44 +0200

    dockerfile and build issue fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,29 @@
 const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.join(__dirname, '/public'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, '/public'),
-    historyApiFallback: true,
-    inline: true,
-    port: 3001,
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js|jsx$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        resolve: {
-          extensions: ['.js', '.jsx'],
-        },
-        query: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        },
-      },
-    ],
-  },
+    entry: './src/index.jsx',
+    output: {
+        path: path.join(__dirname, '/public'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '/public'),
+        historyApiFallback: true,
+        inline: true,
+        port: 3001,
+        hot: true,
+    },
+    module: {
+        rules: [{
+            test: /\.js|jsx$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            resolve: {
+                extensions: ['.js', '.jsx'],
+            },
+            query: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+        }, ],
+    },
 };
