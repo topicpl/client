0de6300f53c57425f448d0bf34ab99c038b4caaa
Libikk <libikpl@gmail.com>
2020-08-26 07:55:07 +0100

    chore: app config

--- appConfig.js
+++ appConfig.js
@@ -3,6 +3,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 const appConfig = {
   serverUrl: isDev ? 'http://localhost:3000' : 'http://localhost:3000',
   isDev,
+  gaTrackingId: isDev ? 'UA-176397412-2' : 'UA-176397412-1',
 };
 
 export default appConfig;
