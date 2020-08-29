fd459536cdc1065662af74a6ee71dc19e9be99c3
ChickenMan4236 <cziken@ARCH.localdomain>
2020-08-29 20:06:51 +0000

    sockets and cors (probably working)

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -8,7 +8,7 @@ const cookies = new Cookies();
 
 let socket;
 if (appConfig.isDev) socket = io('http://localhost:3000');
-else socket = io('http://139.59.137.236:3000');
+else socket = io('https://thetopic.pl', {secure: true});
 let identity;
 let roomSid;
 
