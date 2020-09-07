3e069f257f66770543528f2b79c0e7f58cc3cdd5
Libikk <libikpl@gmail.com>
2020-09-07 19:00:40 +0100

    chore: line break

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use('/', express.static('landing'));
 app.use('/app', express.static('public'));
 app.use('/app/:category', express.static('public'));
+
 app.use('*', express.static('landing/404'));
 
 app.listen(3001, () => console.warn('The Topic app listening on port 3001!'));
