63bbfc881521f5234f063007ede1fddcff847c57
Libikk <libikpl@gmail.com>
2020-10-30 18:50:00 +0000

    chore: add base ui for testing

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Device } from 'mediasoup-client';
 import * as mySignaling from './my-signaling';
-// Our own signaling stuff.
-const sleep = (delayTime) => new Promise((resolve) => setTimeout(() => resolve(), delayTime));
 
 const peerId = `${Math.random().toString()}-participant`;
+
 const Test = () => {
   const [rtpCapabilities, setRtpCapabilities] = useState(null);
-  const [peerIpToReceive, setPeerIpToReceive] = useState('134-f58-a716');
+  const [peerIpToReceive, setPeerIpToReceive] = useState('');
+  const [isInRoom, setIsInRoom] = useState(false);
+
 
   const main = async () => {
+    setIsInRoom(true);
     const device = new Device();
 
     // Communicate with our server app to retrieve router RTP capabilities.
@@ -35,43 +37,27 @@ const Test = () => {
     sendTransport.on('connect', async ({ dtlsParameters }, callback, errback) => {
       // Here we must communicate our local parameters to our remote transport.
       mySignaling.connectTransport({ transportId: sendTransport.id, dtlsParameters })
-        .then(() => callback())// Done in the server, tell our transport.
+        .then(callback)// Done in the server, tell our transport.
         .catch(errback); // Something was wrong in server side.
     });
 
     // Set transport "produce" event handler.
-    sendTransport.on('produce', async ({ kind, rtpParameters, appData }, callback, errback) => {
-      try {
-        const { id } = await mySignaling.sendTrack({ peerId, transportId: sendTransport.id, kind, rtpParameters, appData });
-
-        // Done in the server, pass the response to our transport.
-        callback({ id });
-      } catch (error) {
-        // Something was wrong in server side.
-        errback(error);
-      }
+    sendTransport.on('produce', ({ kind, rtpParameters, appData }, callback, errback) => {
+      mySignaling.sendTrack({ peerId, transportId: sendTransport.id, kind, rtpParameters, appData })
+        .then(callback)
+        .catch(errback);
     });
 
-
-    // Produce our webcam video.
-    // Produce our webcam video.
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     const webcamTrack = stream.getVideoTracks()[0];
-    const webcamProducer = await sendTransport.produce({
-      track: webcamTrack,
-      encodings: [
-        { maxBitrate: 96000, scaleResolutionDownBy: 4 },
-        { maxBitrate: 680000, scaleResolutionDownBy: 1 },
-      ],
-      appData: { mediaTag: 'cam-video' },
-    });
+    const webcamProducer = await sendTransport.produce({ track: webcamTrack, appData: { mediaTag: 'cam-video' } });
   };
 
   const createReceiveTransport = async () => {
     const device = new Device();
     await device.load({ routerRtpCapabilities: rtpCapabilities });
     const { transportOptions } = await mySignaling.createTransport({ direction: 'recv', peerId });
-    console.log('createReceiveTransport -> transportOptions', transportOptions);
+
     const recvTransport = device.createRecvTransport(transportOptions);
 
     recvTransport.on('connect', async ({ dtlsParameters }, callback, errback) => {
@@ -93,13 +79,7 @@ const Test = () => {
 
   const receiveTrack = async () => {
     const recvTransport = await createReceiveTransport();
-
-    const consumerParameters = await mySignaling.receiveTrack({
-      peerId,
-      mediaPeerId: peerId,
-      mediaTag: 'cam-video',
-      rtpCapabilities,
-    })
+    const consumerParameters = await mySignaling.receiveTrack({ peerId, mediaPeerId: peerId, mediaTag: 'cam-video', rtpCapabilities })
       .catch(console.error);
 
     const consumer = await recvTransport.consume({
@@ -107,19 +87,11 @@ const Test = () => {
       appData: { peerId: peerIpToReceive, mediaTag: 'cam-video' },
     });
 
-    console.log('receiveTrack -> recvTransport.connectionState', recvTransport.connectionState);
-    while (recvTransport.connectionState !== 'connected') {
-      console.log('  transport connstate', recvTransport.connectionState);
-      await sleep(100);
-    }
     await mySignaling.resumeCustomer({ consumerId: consumer.id });
 
     const videoEl = document.querySelector('#my-video');
-    videoEl.srcObject = new MediaStream([consumer.track]);
-    videoEl.consumer = consumer;
-    // let's "yield" and return before playing, rather than awaiting on
-    // play() succeeding. play() will not succeed on a producer-paused
-    // track until the producer unpauses.
+    videoEl.srcObject = new MediaStream([consumer.track.clone()]);
+
     videoEl.play()
       .catch((error) => console.warn('audioElem.play() failed:%o', error));
   };
@@ -130,18 +102,28 @@ const Test = () => {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <button onClick={main}>Join</button>
+      <button onClick={main} disabled={isInRoom}>Join</button>
       <div>
         currentParticipant:
         {' '}
-        <span style={{ color: 'red' }}>{peerId}</span>
+        <div>
+          <h1 style={{ color: 'red', padding: '20px', fontSize: '30px', zIndex: 10 }}>
+            <span>
+              my participant id:
+              {' '}
+              <input style={{ padding: '10px', width: '400px' }} value={peerId} readOnly />
+            </span>
+          </h1>
+        </div>
       </div>
       <input onChange={(e) => setPeerIpToReceive(e.target.value)} />
       <button onClick={receiveTrack}>
         receive
       </button>
       <div style={{ color: 'green' }}>
-        { peerIpToReceive }
+        <span style={{ padding: '10px' }}>
+          { peerIpToReceive }
+        </span>
       </div>
       <video width="320" height="240" autoPlay playsInline id="my-video" controls />
     </div>
